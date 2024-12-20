939cba91c5117f6a2ac5ef90931f78e08cd138b7
Macbook Pro <macbookpro@mbp-de-macbook.home>
2024-12-20 13:55:04 +0100

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+import nextMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
+import rehypePrism from "@mapbox/rehype-prism";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["images.unsplash.com", "res.cloudinary.com"], // Ajoute le domaine Unsplash
+  },
+  experimental: {
+    mdxRs: true,
+  },
+};
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypePrism],
+  },
+});
+
+export default withMDX(nextConfig);
